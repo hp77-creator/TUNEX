13566d2bda179d29c775fad64bc38f8fb9c0101b
Himanshu Pandey <hp77codeforgood@gmail.com>
2021-10-14 02:13:35 +0530

    Remove unnecessary folder
    Signed-off-by: Himanshu Pandey <hp77codeforgood@gmail.com>

--- web/backend/prediction/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('predict/', views.predict, name='predict'),
-    path('form/', views.form_view, name='form'),
-    path('livefeed', views.livefeed, name='livefeed')
-]
